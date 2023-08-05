baf556aaed22c80cd544e7bc9f18eed7115c67e6
Diego Luna <diegocheuquel17@hotmail.com>
2023-08-05 14:16:02 -0300

    Implementación Springboot Maven 3.0.9

---
+++ src/test/java/com/portfolio/DL/DlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.DL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
